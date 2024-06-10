b3b47f8507c1114eb9f0ccd6696cae1cd290a8b4
fikrialfjri <fikribusiness2021@gmail.com>
2024-06-10 23:44:16 +0700

    finishing

--- frontend/src/components/Statistics.jsx
+++ frontend/src/components/Statistics.jsx
@@ -11,31 +11,31 @@ const Statistics = ({ height }) => {
   return (
     <div className="flex flex-col gap-3 justify-evenly" style={{ height: height }}>
       <div className="space-y-2">
-        <label className="text-lg text-gray-500 font-normal">Total Penjualan</label>
+        <label className="text-lg font-normal text-gray-500">Total Penjualan</label>
         <div className="flex items-center justify-between gap-3">
-          <h3 className="text-4xl font-semibold text-neutral-900">{transformedData?.total_sales?.toLocaleString()}</h3>
-          <div className="h-11 w-11 aspect-square flex items-center justify-center rounded-full bg-primary-50">
-            <FaChartSimple className="text-primary-300 w-full" />
+          <h3 className="text-3xl font-semibold text-neutral-900">{transformedData?.total_sales?.toLocaleString() || 0}</h3>
+          <div className="flex items-center justify-center rounded-full h-11 w-11 aspect-square bg-primary-50">
+            <FaChartSimple className="w-full text-primary-300" />
           </div>
         </div>
       </div>
       <div className="h-[1px] w-full bg-neutral-300" />
       <div className="space-y-2">
-        <label className="text-lg text-gray-500 font-normal">Total Pendapatan (dalam Miliar)</label>
+        <label className="text-lg font-normal text-gray-500">Total Pendapatan (dalam Miliar)</label>
         <div className="flex items-center justify-between gap-3">
-          <h3 className="text-4xl font-semibold text-neutral-900">Rp{(transformedData?.total_revenue / 1000000000)?.toLocaleString()}</h3>
-          <div className="h-11 w-11 aspect-square flex items-center justify-center rounded-full bg-primary-50">
-            <FaMoneyBills className="text-primary-300 w-full" />
+          <h3 className="text-3xl font-semibold text-neutral-900">Rp{(transformedData?.total_revenue / 1000000000 || 0)?.toLocaleString()}</h3>
+          <div className="flex items-center justify-center rounded-full h-11 w-11 aspect-square bg-primary-50">
+            <FaMoneyBills className="w-full text-primary-300" />
           </div>
         </div>
       </div>
       <div className="h-[1px] w-full bg-neutral-300" />
       <div className="space-y-2">
-        <label className="text-lg text-gray-500 font-normal">Produk Terlaris</label>
+        <label className="text-lg font-normal text-gray-500">Produk Terlaris</label>
         <div className="flex items-center justify-between gap-3">
-          <h3 className="text-4xl font-semibold text-neutral-900">{transformedData?.most_sold_product}</h3>
-          <div className="h-11 w-11 aspect-square flex items-center justify-center rounded-full bg-primary-50">
-            <FaArrowTrendUp className="text-primary-300 w-full" />
+          <h3 className="text-3xl font-semibold text-neutral-900">{transformedData?.most_sold_product || "No data"}</h3>
+          <div className="flex items-center justify-center rounded-full h-11 w-11 aspect-square bg-primary-50">
+            <FaArrowTrendUp className="w-full text-primary-300" />
           </div>
         </div>
       </div>
